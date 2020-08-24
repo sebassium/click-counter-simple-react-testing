5113ba0b48744bae2006ea694b60a30a291ebe05
Sebastian Hernandez <sebhernandezram@gmail.com>
2020-08-23 20:45:05 -0500

    Refactor App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       counter: 0,
+      showError: false,
     };
+
+    this.handleIncrement = this.handleIncrement.bind(this);
+    this.handleDecrement = this.handleDecrement.bind(this);
   }
+
+  handleIncrement = () => {
+    if (this.state.showError) {
+      this.setState({ ...this.state, showError: false });
+    } else {
+      this.setState({ ...this.state, counter: this.state.counter + 1 });
+    }
+  };
+
+  handleDecrement = () => {
+    if (this.state.counter === 0) {
+      this.setState({ ...this.state, showError: true });
+    } else {
+      this.setState({ ...this.state, counter: this.state.counter - 1 });
+    }
+  };
 
   render() {
     return (
@@ -15,16 +34,15 @@ class App extends Component {
         <h1 data-test="counter-display">
           The counter is currently: {this.state.counter}
         </h1>
-        <button
-          data-test="increment-button"
-          onClick={() => this.setState({ counter: this.state.counter + 1 })}
-        >
+        {this.state.showError && (
+          <p data-test="error-message" style={{ color: 'red' }}>
+            Error: counter should not be less than 0
+          </p>
+        )}
+        <button data-test="increment-button" onClick={this.handleIncrement}>
           Increment
         </button>
-        <button
-          data-test="decrement-button"
-          onClick={() => this.setState({ counter: this.state.counter - 1 })}
-        >
+        <button data-test="decrement-button" onClick={this.handleDecrement}>
           Decrement
         </button>
       </div>
--- src/App.test.js
+++ src/App.test.js
@@ -65,7 +65,7 @@ test('clicking button increments counting display', () => {
   expect(counterDisplay.text()).toContain(counter + 1);
 });
 
-test('clicking button decrements conting display', () => {
+test('clicking button decrements counting display', () => {
   const counter = 7;
   const wrapper = setup(null, { counter });
 
@@ -77,3 +77,85 @@ test('clicking button decrements conting display', () => {
   const counterDisplay = findByTestAttr(wrapper, 'counter-display');
   expect(counterDisplay.text()).toContain(counter - 1);
 });
+
+test('stop decrementing counter when it gets to cero', () => {
+  const wrapper = setup();
+
+  // find button and click
+  const decrementButton = findByTestAttr(wrapper, 'decrement-button');
+  decrementButton.simulate('click');
+
+  // find display and test value
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.text()).toContain(0);
+});
+
+test("don't show error if it's not needed", () => {
+  const wrapper = setup();
+  const errorMessage = findByTestAttr(wrapper, 'error-message');
+  expect(errorMessage.length).toBe(0);
+});
+
+test('show an error when trying to decrement counter below 0', () => {
+  const wrapper = setup();
+
+  // find button and click
+  const decrementButton = findByTestAttr(wrapper, 'decrement-button');
+  decrementButton.simulate('click');
+
+  // Error display should appear
+  const errorMessage = findByTestAttr(wrapper, 'error-message');
+  expect(errorMessage.length).toBe(1);
+});
+
+test('disappear error when incrementing count from 0', () => {
+  const wrapper = setup();
+
+  // find button and click to decrement and show error
+  const decrementButton = findByTestAttr(wrapper, 'decrement-button');
+  decrementButton.simulate('click');
+
+  // find button and click to increment
+  const incrementButton = findByTestAttr(wrapper, 'increment-button');
+  incrementButton.simulate('click');
+
+  // Error display should desappear
+  const errorMessage = findByTestAttr(wrapper, 'error-message');
+  expect(errorMessage.length).toBe(0);
+});
+
+// describe('counter is 0 and decrement is clicked', () => {
+//   // using a describe here so I can use a "beforeEach" for shared setup
+
+//   // scoping wrapper to the describe, so it can be used in beforeEach and the tests
+//   let wrapper
+//   beforeEach(() => {
+//     // no need to set counter value here; default value of 0 is good
+//     wrapper = setup();
+
+//     // find button and click
+//     const button = findByTestAttr(wrapper, 'decrement-button');
+//     button.simulate('click');
+//     wrapper.update();
+//   });
+//   test('error shows', () => {
+//     // check the class of the error message
+//     const errorDiv = findByTestAttr(wrapper, 'error-message');
+//     const errorHasHiddenClass = errorDiv.hasClass('hidden');
+//     expect(errorHasHiddenClass).toBe(false);
+//   });
+//   test('counter still displays 0', () => {
+//     const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+//     expect(counterDisplay.text()).toContain(0);
+//   });
+//   test('clicking increment clears the error', () => {
+//     // find and click the increment button
+//     const button = findByTestAttr(wrapper, 'increment-button');
+//     button.simulate('click');
+
+//     // check the class of the error message
+//     const errorDiv = findByTestAttr(wrapper, 'error-message');
+//     const errorHasHiddenClass = errorDiv.hasClass('hidden');
+//     expect(errorHasHiddenClass).toBe(true);
+//   });
+// });
